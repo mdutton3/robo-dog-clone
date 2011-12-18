5e82f6f2bf8a965a52917939fed623ebe688b67a
Matthew Dutton <MatthewRDutton@gmail.com>
2011-12-18 14:13:44 -0500

    Use ros param for HSV limits; Reformatted

--- src/FilterRGBD.cpp
+++ src/FilterRGBD.cpp
@@ -29,12 +29,24 @@ using pcl::PointXYZRGB;
 using pcl::PointCloud;
 
 //typedef PointXYZRGB PointType;
-typedef PointCloud< PointXYZ > PointCloudXYZ;
-typedef PointCloud< PointXYZRGB > PointCloudRGB;
-
+typedef PointCloud<PointXYZ> PointCloudXYZ;
+typedef PointCloud<PointXYZRGB> PointCloudRGB;
 
 struct HSV
 {
+	inline HSV() :
+		h(0), s(0), v(0)
+	{
+	}
+	inline HSV(uint8_t h_, uint8_t s_, uint8_t v_) :
+		h(h_), s(s_), v(v_)
+	{
+	}
+	inline HSV(HSV const & rhs) :
+		h(rhs.h), s(rhs.s), v(rhs.v)
+	{
+	}
+
 	uint8_t h;
 	uint8_t s;
 	uint8_t v;
@@ -46,27 +58,42 @@ class FilterRGBD
 	ros::Subscriber pc_sub;
 	ros::Publisher pc_pub;
 	ros::Publisher centroid_pub;
-	CvScalar const ball_hsv_min;
-	CvScalar const ball_hsv_max;
+	HSV hsv_min;
+	HSV hsv_max;
 
 	Eigen::Vector4f centroid_filtered;
 
 public:
 	FilterRGBD() :
-		nh_("~"), ball_hsv_min(cvScalar(20, 110, 110, 0)), ball_hsv_max(
-				cvScalar(45, 256, 256, 0))
+		nh_("~"), hsv_min(20, 110, 110), hsv_max(45, 255, 255)
 	{
 		pc_sub = nh_.subscribe("in", 1, &FilterRGBD::OnPC, this);
 		pc_pub = nh_.advertise<PointCloudRGB> ("out", 10);
 		centroid_pub = nh_.advertise<PointCloudXYZ> ("centroid", 10);
+
+		int temp;
+		if (nh_.getParam("hue_min", temp))
+			hsv_min.h = temp;
+		if (nh_.getParam("sat_min", temp))
+			hsv_min.s = temp;
+		if (nh_.getParam("val_min", temp))
+			hsv_min.v = temp;
+
+		if (nh_.getParam("hue_max", temp))
+			hsv_max.h = temp;
+		if (nh_.getParam("sat_max", temp))
+			hsv_max.s = temp;
+		if (nh_.getParam("val_max", temp))
+			hsv_max.v = temp;
 	}
 
 	~FilterRGBD()
 	{
 	}
 
-	void OnPC( PointCloudRGB const & pc )
+	void OnPC(PointCloudRGB const & pc)
 	{
+		ROS_DEBUG( "Received PC of size: %zu", pc.points.size() );
 
 		std::vector<size_t> indices;
 		PointCloudXYZ::Ptr cloud_filtered(new PointCloudXYZ);
@@ -82,14 +109,14 @@ public:
 			if (!(pt.x == pt.x))
 				continue;
 
-			if ((hsv.h < ball_hsv_min.val[0]) || (hsv.h > ball_hsv_max.val[0]))
+			if ((hsv.h < hsv_min.h) || (hsv.h > hsv_max.h))
 				continue;
-			if ((hsv.s < ball_hsv_min.val[1]) || (hsv.s > ball_hsv_max.val[1]))
+			if ((hsv.s < hsv_min.s) || (hsv.s > hsv_max.s))
 				continue;
-			if ((hsv.v < ball_hsv_min.val[2]) || (hsv.v > ball_hsv_max.val[2]))
+			if ((hsv.v < hsv_min.v) || (hsv.v > hsv_max.v))
 				continue;
 
-			cloud_filtered->push_back( PointXYZ(pt.x, pt.y, pt.z) );
+			cloud_filtered->push_back(PointXYZ(pt.x, pt.y, pt.z));
 			indices.push_back(i);
 		}
 
@@ -138,19 +165,21 @@ public:
 		pc_pub.publish(out);
 
 		Eigen::Vector4f centroid_sample;
-		pcl::compute3DCentroid( out, centroid_sample );
+		pcl::compute3DCentroid(out, centroid_sample);
 
 		static double const sample_weight = 0.5;
-		centroid_filtered = centroid_filtered * (1.0 - sample_weight) + centroid_sample * sample_weight;
+		centroid_filtered = centroid_filtered * (1.0 - sample_weight)
+				+ centroid_sample * sample_weight;
 
 		PointCloudXYZ centroid_pc;
-		centroid_pc.push_back( pcl::PointXYZ( centroid_filtered[0], centroid_filtered[1], centroid_filtered[2] ) );
+		centroid_pc.push_back(pcl::PointXYZ(centroid_filtered[0],
+				centroid_filtered[1], centroid_filtered[2]));
 		centroid_pc.header = pc.header;
 		centroid_pc.height = centroid_pc.width = 1;
 		centroid_pc.is_dense = true;
 		centroid_pc.sensor_orientation_ = pc.sensor_orientation_;
 		centroid_pc.sensor_origin_ = pc.sensor_origin_;
-		centroid_pub.publish( centroid_pc );
+		centroid_pub.publish(centroid_pc);
 
 	}
 };
